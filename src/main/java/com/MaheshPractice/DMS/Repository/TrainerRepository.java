deae332855968313bef3a7d8c52a25468c69e619
Maheshwar <S559173@nwmissouri.edu>
2023-11-25 14:35:26 -0600

    added files

---
+++ src/main/java/com/MaheshPractice/DMS/Repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.MaheshPractice.DMS.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.MaheshPractice.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
